92f11a4606078e41e77fbfaecb3edea47fed0b50
Aki_Yasmin <66842642+AkiYasmin@users.noreply.github.com>
2020-07-21 16:29:02 -0400

    Update StellarWitch.cs

--- StellarWitch.cs
+++ StellarWitch.cs
@@ -40,7 +40,7 @@ namespace StellarWitch{
 	public Textbox();
 		public void CreateTextBox() {
    		TextBox textBox1 = new TextBox();
-    		textBox1.Font = new Font ("Arial" , 20);
+    		textBox1.Font = new Font ("Arial" , 12);
     		textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
     		textBox1.Height = textBox1.PreferredHeight;
 		textBox1.text="BEEP BEEP BEEP.*The sound of an alarm clock rings througout the room*";
