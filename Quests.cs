f7f4d0812ed0474a0f399ff181e363b5fdce3840
Aki_Yasmin <66842642+AkiYasmin@users.noreply.github.com>
2020-08-04 23:49:13 -0500

    Add files via upload

---
+++ Quests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Quests
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public Item RewardItem { get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
+
+
+        public Quests(int id,string name, string description,int rewardExpP,int rewardGold)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+            RewardExperiencePoints = rewardExpP;
+            RewardGold = rewardGold;
+
+            QuestCompletionItems = new List<QuestCompletionItem>();
+        }
+    }
+}
