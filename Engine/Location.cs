5745aa5d017c967c1ea3d181be84928087fbf722
Aki_Yasmin <66842642+AkiYasmin@users.noreply.github.com>
2020-08-04 23:39:53 -0500

    Add files via upload

---
+++ Engine/Location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Item ItemRequiredToEnter { get; set; }
+        public Quests QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
+
+        public Location(int id,string name,string description,Item itemRequiredToEnter = null, Quests questAvailableHere = null, Monster monsterLivingHere = null)
+        {
+            ID = id;
+            name = Name;
+            Description = description;
+            ItemRequiredToEnter = itemRequiredToEnter;
+            QuestAvailableHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
+
+        }
+
+    }
+}
